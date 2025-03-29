ee262bef3d0273ff2e823b46ccc3b0c0c89c7785
JaredFagg <jaredfagg03@outlook.com>
2025-03-29 23:20:57 +1000

    Started 3D integration - Blender Scene
    Includes blender scene assets and keyboard with custom materials. No interaction yet.

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,85 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+let controls;
+let scene;
+let camera;
+let renderer;
 
 function main() {
+	// Establishes ThreeJS Canvas, Renderer, Camera & Scene.
 	const canvas = document.getElementById("ThreeJSCanvas");
-	const renderer = new THREE.WebGLRenderer( { antialias: true, canvas } );
+	camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+	renderer = new THREE.WebGLRenderer( { antialias: true, canvas } );
+	scene = new THREE.Scene();
 
-	const fov = 75;
-	const aspect = 1;
-	const near = 0.1;
-	const far = 5;
-	const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
-	camera.position.x = -2; camera.position.y = 2; camera.position.z = 2;  
-    camera.lookAt(0, 0, 0);
+	// Sets correct pixel density using resolution of computer monitor
+	renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize(window.innerWidth, window.innerHeight);
 
-	const scene = new THREE.Scene();
+	modifyScene(scene, camera);
+	//helper(scene, camera, renderer);
+	createScene(scene);
+}
 
-	const boxWidth = 1;
-	const boxHeight = 1;
-	const boxDepth = 1;
-	const geometry = new THREE.BoxGeometry( boxWidth, boxHeight, boxDepth );
+function modifyScene(sc, c) {
+	sc.background = new THREE.Color().setRGB(1,1,1);
+	c.position.set(1.8, 2.4, 0);
+	c.lookAt(0, 0, 0);
+}
 
-	const material = new THREE.MeshPhongMaterial( { color: 0xFFFFFF } ); // greenish blue
+function createScene(sc) {
+	// Creates a Light
+	const pointLight = new THREE.PointLight(0xffffff, 500, 100);
+	pointLight.position.set(6, 15, 15);
+	
+	const ambientLight = new THREE.AmbientLight(0xffffff);
+	scene.add(pointLight, ambientLight);
 
-	const cube = new THREE.Mesh( geometry, material );
-	scene.add( cube );
+	const gltfLoader = new GLTFLoader();
+	gltfLoader.load('../assets/models/Keyboard_Model.glb', function ( gltf) {
+		const model = gltf.scene;
+		const modelMaterial = new THREE.MeshPhongMaterial({
+			color: 0x1f1f1f
+		});
+		model.traverse((o) => {
+			if(o.isMesh) {
+				o.material = modelMaterial;
+			}
+		})
 
-    const light = new THREE.DirectionalLight(0xFFFFFF, 2);
-    light.position.set(2, 2, 2);
-    scene.add(light);
+		sc.add(model);
+	});
 
-	renderer.render( scene, camera );
+	gltfLoader.load('../assets/models/Keyboard_Scene.glb', function ( gltf) {
+		const scene = gltf.scene;
+		const sceneMaterial = new THREE.MeshPhongMaterial({
+			color: 0xffffff
+		});
+		scene.traverse((o) => {
+			if(o.isMesh) {
+				o.material = sceneMaterial;
+			}
+		})
+
+		sc.add(scene);
+	});
 
 }
 
+function helper(sc, c, r) {
+	// Creates a grid and camera system to orbit and pan during development
+	const gridHelper = new THREE.GridHelper(200, 50);
+	sc.add(gridHelper);
+	controls = new OrbitControls(c, r.domElement);
+}
+
+function update() {
+	requestAnimationFrame(update);
+	//controls.update();
+	renderer.render(scene, camera);
+}
+
 main();
+update();
